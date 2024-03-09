b211252b0cc36e6048baa702c5002308c060ba0d
sbin <sbinha123@gmail.com>
2024-03-09 21:46:41 +0900

    chore: toolbar 코드수정

--- apps/web/components/editor/workspace/toolbar/panel/panel.tsx
+++ apps/web/components/editor/workspace/toolbar/panel/panel.tsx
@@ -1,41 +1,27 @@
-import { useCallback } from 'react';
 import {
-  PanelBackground,
-  PanelText,
-  PanelMedia,
   PanelAnimations,
-  PanelTransitions,
   PanelAudio,
+  PanelBackground,
+  PanelMedia,
+  PanelText,
+  PanelTransitions,
 } from '.';
 
-import { useToolbarStore, panelValue } from '@/app/store/use-toolbar-store';
-import type { PanelType } from '@/app/store/use-toolbar-store';
+import { panelValue, useToolbarStore } from '@/app/store/use-toolbar-store';
+
+const panelMap = {
+  [panelValue.background]: <PanelBackground />,
+  [panelValue.text]: <PanelText />,
+  [panelValue.media]: <PanelMedia />,
+  [panelValue.animations]: <PanelAnimations />,
+  [panelValue.transitions]: <PanelTransitions />,
+  [panelValue.audio]: <PanelAudio />,
+};
 
 export default function Panel() {
   const { panelType } = useToolbarStore();
 
-  const renderPanel = useCallback((panel: PanelType) => {
-    switch (panel) {
-      case panelValue.background:
-        return <PanelBackground />;
-      case panelValue.text:
-        return <PanelText />;
-      case panelValue.media:
-        return <PanelMedia />;
-      case panelValue.animations:
-        return <PanelAnimations />;
-      case panelValue.transitions:
-        return <PanelTransitions />;
-      case panelValue.audio:
-        return <PanelAudio />;
-      default:
-        return <PanelBackground />;
-    }
-  }, []);
-
   return (
-    <div className="p-4 flex-1 bg-white rounded-md">
-      {renderPanel(panelType)}
-    </div>
+    <div className="p-4 flex-1 bg-white rounded-md">{panelMap[panelType]}</div>
   );
 }
